d1c31e0289d174d28de55d44bbfef07924bad7ba
pjo <pjo@dbc.dk>
2020-05-05 15:05:28 +0200

    struggling with run script ..

--- docker/www/script/run_start.sh
+++ docker/www/script/run_start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-# environment
-source .env
 # trusted_host_patterns
 # sed -i "s/@DOMAIN@/$APACHE_SERVER_NAME/" $SETTINGS
 # database settings
 cd /var/www || return
-# Insert the database settings into settings.php.
-vendor/bin/drupal dba --password=$POSTGRES_PASSWORD --driver=pgsql --host=$POSTGRES_HOST --database=$POSTGRES_DB \
+# source env vars and Insert the database settings into settings.php.
+source /.env && vendor/bin/drupal dba --password=$POSTGRES_PASSWORD --driver=pgsql --host=$POSTGRES_HOST --database=$POSTGRES_DB \
 --username=$POSTGRES_USER --port=5432 --default -n
