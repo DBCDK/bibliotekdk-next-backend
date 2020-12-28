bb5eea5cff5f157c15a8d6255fa5606f78c34f57
pjo <pjo@dbc.dk>
2020-12-28 14:47:10 +0100

    escape

--- script/config_memcache.sh
+++ script/config_memcache.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 SETTINGS=/var/www/web/sites/default/settings.php
 # memcache settings
-MEMCACHE="$settings['memcache']['servers'] = ['bibdk-backend-memcached-master:11211' => 'default']; \
-$settings['memcache']['bins'] = ['default' => 'default']; \
-$settings['memcache']['key_prefix'] = ''; \
-$settings['cache']['default'] = 'cache.backend.memcache'; \
-$settings['cache']['bins']['render'] = 'cache.backend.memcache';"
+MEMCACHE="\$settings['memcache']['servers'] = ['bibdk-backend-memcached-master:11211' => 'default']; \
+\$settings['memcache']['bins'] = ['default' => 'default']; \
+\$settings['memcache']['key_prefix'] = ''; \
+\$settings['cache']['default'] = 'cache.backend.memcache'; \
+\$settings['cache']['bins']['render'] = 'cache.backend.memcache';"
 
 echo $MEMCACHE
 # sed -i "s/@DOMAIN@/$APACHE_SERVER_NAME/" $SETTINGS
