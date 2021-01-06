7ca177e66773cc65a734d91db5b1c8261cbc4f21
pjo <pjo@dbc.dk>
2021-01-06 09:21:40 +0100

    also sync files folder if in staging namespace

--- script/run_master_start.sh
+++ script/run_master_start.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 # trusted_host_patterns
 # sed -i "s/@DOMAIN@/$APACHE_SERVER_NAME/" $SETTINGS
+
+# get files from prod if in staging namespace
+cd /tmp || return
+if [ "$NAMESPACE_NAME" == 'frontend-staging' ]; then
+  # Fetch the files folder from prod.
+  tar -xf files.tar.gz
+  rm -rf /var/www/web/sites/default/files/*
+  cp -Rf files /var/www/web/sites/default
+  chown -Rf www-data:www-data /var/www/web/sites/default/files
+  rm -rf files files.tar.gz
+fi
 # database settings
 cd /var/www || return
 ls -la
