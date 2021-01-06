449153f436070a2f9bac29d4664ddddb2d5d2ba8
pjo <pjo@dbc.dk>
2021-01-06 09:55:33 +0100

    correct comment

--- script/run_master_start.sh
+++ script/run_master_start.sh
@@ -15,6 +15,6 @@ fi
 # database settings
 cd /var/www || return
 ls -la
-# source env vars and Insert the database settings into settings.php.
+# Insert the database settings into settings.php.
 vendor/bin/drupal dba --password=$POSTGRES_PASSWORD --driver=pgsql --host=$POSTGRES_HOST \
  --database=$POSTGRES_DB --username=$POSTGRES_USER --port=5432 --default -n
